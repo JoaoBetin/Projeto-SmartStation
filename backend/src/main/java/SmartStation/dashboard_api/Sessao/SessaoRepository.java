2c7bfcd5e30cc084296e5476293eddb83d91bcb9
Joao Betin <betinjoao2@gmail.com>
2026-03-15 01:50:49 -0300

    feat: adicionado as sessoes

---
+++ backend/src/main/java/SmartStation/dashboard_api/Sessao/SessaoRepository.java
@@ -0,0 +1,6 @@
+package SmartStation.dashboard_api.Sessao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessaoRepository extends JpaRepository<SessaoModel, Long> {
+}
